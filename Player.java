ceacea3a53a1ca84a479dcf0a4da234137180937
Clayton Thomas <ct@Claytons-Air.T-mobile.com>
2018-01-29 23:02:47 -0700

    added toString, hashCode, and equals methods

--- Player.java
+++ Player.java
@@ -1,17 +1,52 @@
 import java.io.Serializable;
 
 public class Player implements Serializable{
-
+  //attribute
   private string name;
-
+  //constructor
   public Player(){}
-
+  //getter
   public string getName(){
     return name;
   }
-
+  //setter
   public void setName(string name){
     Player.name = name;
+  }
+
+  // i don't know what the rest of this means
+  @Override
+  public String toString(){
+    return "Player{" + "name=" + name + ", bestTime=" + bestTime + "}";
+  }
+
+  @Override
+  public int hashCode(){
+    int hash = 3;
+    hash = 47 + hash + Objects.hashCode(this.name);
+    hash = 47 + hash +
+      (int) (Double.doubleToLongBits(this.bestTime) ^
+      (Double.doubleToLongBits(this.bestTime) >>> 32));
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj){
+    if (obj == null){
+      return false;
+    }
+    if (getClass() != obj.Class()){
+      return false;
+    }
+    final Player other = (Player) obj;
+    if (!Objects.equals(this.name, other.name)){
+      return false;
+    }
+    if (Double.doubleToLongBits(this.bestTime) !=
+    Double.doubleToLongBits(other.bestTime)){
+      return false;
+    }
+    return true;
   }
 
 }
