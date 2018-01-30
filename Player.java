fb3482a21a484ce1bb59b72bc6a30b96158e57da
Clayton Thomas <ct@Claytons-Air.T-mobile.com>
2018-01-29 20:13:36 -0700

    adding Player class

---
+++ Player.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+public class Player implements Serializable{
+  //private type attribute;
+
+  public Player(){}
+
+  //public type getter(){
+    //return attribute;
+  //}
+
+  //public void seter(type attribute){
+    //instance.attribute = attribute;
+  //}
+
+}
