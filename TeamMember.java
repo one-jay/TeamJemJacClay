c8237c4a54f9fb2c75766112a2ebb85ec7f024f4
Clayton Thomas <ct@Claytons-Air.T-mobile.com>
2018-01-31 00:15:04 -0700

    updated syntax errors found on compile

--- TeamMember.java
+++ TeamMember.java
@@ -2,22 +2,26 @@
 
 import java.io.Serializable;
 
-public enum TeamMember implements Serializable{
+//if this class should be an enum,
+//do we need a list of the TeamMember names and titles?
+public class TeamMember implements Serializable{
 
   private String name;
   private String title;
 
   public TeamMember(){}
-
+//also, if this class should be an enum,
+//then there shouldn't be any setter methods
   public String getName(){
     return name;
   }
   public void setName(String name){
     this.name = name;
-
-  public String getName(){
-    return name;
+  }
+  public String getTitle(){
+    return title;
   }
   public void setTitle(String title){
     this.title = title;
+  }
 }
